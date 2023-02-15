8346e66724d9335b3e2f8277b20704772964362a
Matthias <matthias.verbeeke@student.hu.nl>
2023-02-15 15:14:00 +0100

    loading gemaakt

---
+++ Scrapbook/Loading/loading.py
@@ -0,0 +1,30 @@
+a = 0
+b = 100
+max = b
+direction = 'up'
+
+while i != -1:
+    i = 0
+    while i < a:
+        print (" ",end="")
+        i += 1
+    
+    print("*****",end="")
+    
+    i = 0
+    while i < b:
+        print (" ",end="")
+        i += 1
+    
+    if direction == 'up':
+        a += 1
+        b -= 1
+    elif direction == 'down':
+        a -= 1
+        b += 1
+
+    if a == max:
+        direction = 'down'
+    elif b == max:
+        direction = 'up'
+    print ("")
