46b2e62064172d55685eb8b3f907450b9ddc9051
Matthias <matthias.verbeeke@student.hu.nl>
2023-02-22 14:29:36 +0100

    Practice questions toegevoegd

---
+++ Week3/Practice question 6.py
@@ -0,0 +1,42 @@
+# Q1
+# Allows adding characters into strings
+
+# Q2
+# \n is new line
+# \t  is tab
+
+# Q3
+# print (r,'\')
+
+# Q4
+# Because the string is in double quotes
+
+# Q5
+# print ("""
+# 
+# 
+# """)
+
+# Q6
+# 'Hello, world!'[1] = e
+# 'Hello, world!'[0:5] = Hello
+# 'Hello, world!'[:5] = Hello
+# 'Hello, world!'[3:] = lo, world!
+
+# Q7
+# 'Hello'.upper() = HELLO
+# 'Hello'.upper().isupper() = True
+# 'Hello'.upper().lower() = hello
+
+# Q8
+#'Remember, remember, the fifth of November.'.split() = ['Remember,', 'remember,', 'the', 'fifth', 'of', 'November.']
+# '-'.join('There can be only one.'.split()) = There-can-be-only-one.
+
+# Q9
+# string.center()
+# string.ljust()
+# string.rjust()
+
+# Q10
+# string.lstrip()
+# string.rstrip()
