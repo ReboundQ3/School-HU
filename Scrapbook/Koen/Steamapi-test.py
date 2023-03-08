f7067025cc28ec49adbe92664a65250b6132049e
Koen Bolte <Koen.Bolte@student.hu.nl>
2023-03-08 14:17:39 +0100

    Haalt nu profieldata op

--- Scrapbook/Koen/Steamapi-test.py
+++ Scrapbook/Koen/Steamapi-test.py
@@ -8,12 +8,16 @@ import json
 #respone_API = requests.get('http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=281990&key=3B9518C4F0770B32EC2AB7A6B635E077&steamid=76561198044252418&format=json')
 #print(respone_API.status_code)
 
-
-url = "http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=281990&key=3B9518C4F0770B32EC2AB7A6B635E077&steamid=76561198044252418&format=json"
+#Stellaris game Achievements
+#url = "http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=281990&key=3B9518C4F0770B32EC2AB7A6B635E077&steamid=76561198044252418&format=json"
+#Playerdata Koen's account (ReboundQ3)
+url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=3B9518C4F0770B32EC2AB7A6B635E077&steamids=76561198044252418&format=json"
 
 response = urlopen(url)
 
 data_json = json.loads(response.read())
 
 print(data_json)
+
+
 # %%
