01b6fd5a699638f6303122b98a76e0b4952f122c
Matthias Verbeeke <matthias.verbeeke@student.hu.nl>
2023-03-26 13:30:47 +0200

    Commit

--- Scrapbook/Mathias/insert.py
+++ Scrapbook/Mathias/insert.py
@@ -1,11 +1,30 @@
+import urllib as url
+import urllib.request
+from urllib.request import urlopen
+import json
+import pprint
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+import os
 
-vandaag_afgk = "10/10/2022"
-for game in gamelist:
-    game_id  = game[0]  
-    game_name = game[1]
-    playercount = "891398" # Ophalen via api
-    ql = ''' INSERT INTO tbl_stats(Date,Game_id,Game_name,Playercount)
-              VALUES(vandaag_afgk,game_id,game_name,playercount) '''
+# Koen Git credentials z6ugwyvaic3wp5ujr3ftullejjc3hs4aggdade3qrge32p2jhhpa
+# Koen Token pybu6qltnuke3dxkbsd5m2thxzvqi2q6eal3flmmhigk3dse2b4q
+steamToken = "F9DF95CC74A3D0C07A6A83C7D0C66F6A"
+steamID = "76561198121931212"
+APIUrl = "http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=281990&key=" + steamToken +"&steamid=" + steamID +"&format=json"
+# API RESPONSE CHECKER 200 == OK
+respone_API = urlopen(APIUrl)
+#print(respone_API.getcode())
+respone_API_code = respone_API.getcode()
+
+if respone_API_code == 200:
+    print("> STEAM API OK")
+else:
+    print("> STEAM API ERROR")
 
 
-
+allGameURL = "https://api.steampowered.com/ISteamApps/GetAppList/v2/?gameid="
+response = urlopen(allGameURL)
+data_json = json.loads(response.read())['applist']['apps']
+print (data_json)
