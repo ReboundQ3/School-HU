7e9925d09e660ffdc4955f98e6c96159bbfde051
Matthias Verbeeke <matthias.verbeeke@student.hu.nl>
2023-03-26 13:15:03 +0200

    Commit

--- Project/Steam_Stat_Grabber.py
+++ Project/Steam_Stat_Grabber.py
@@ -23,10 +23,17 @@ if path.exists("steamdb.db"):
 else:
     print("> STEAM DB DOES NOT EXIST")
 
-DB_CUR.execute("DELETE FROM tbl_stats")
-DB_CUR.execute("INSERT INTO tbl_stats(game_id, game_name) SELECT game_id, game_name FROM tbl_games")
-DB_CUR.execute("UPDATE tbl_stats SET date = (?)", (vandaag_afgk,)), "WHERE date is NULL"
-DB_CON.commit()
+# DB_CUR.execute("DELETE FROM tbl_stats")
+# DB_CUR.execute("INSERT INTO tbl_stats(game_id, game_name) SELECT game_id, game_name FROM tbl_games")
+# DB_CUR.execute("UPDATE tbl_stats SET date = (?)", (vandaag_afgk,)), "WHERE date is NULL"
+# DB_CON.commit()
+
+gameslist = DB_CUR.execute("SELECT game_id, game_name from tbl_games")
+for game in gamelist:
+    game_id  = game[0]
+    game_name = game[1]
+    playercount = "891398" # Ophalen via api
+    DB_CUR.execute("INSERT INTO tbl_stats(Date,Game_id,Game_name,Playercount) VALUES", (vandaag_afgk,game_id,game_name,playercount))
 
 game_id = DB_CUR.execute("SELECT game_id FROM tbl_stats;")
 print(game_id)
