c1fccff0198c9f6bf37908e3cc9925f0efd1220a
Rebound <ReboundQ1@proton.me>
2024-10-22 21:48:51 +0200

    V2

--- Automation/Week 2/Opdracht 3/sign_by_bob.py
+++ Automation/Week 2/Opdracht 3/sign_by_bob.py
@@ -4,11 +4,11 @@ from Crypto.Signature import pkcs1_15
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
 import hashlib
+from cryptography.fernet import Fernet
 
 with open("message.txt", "rb") as message_data:
     message = message_data.read()
     print(message)
-
     
 if os.path.isfile("./private.pem") == False:
     key = RSA.generate(2048)
@@ -16,6 +16,7 @@ if os.path.isfile("./private.pem") == False:
     with open("private.pem", "wb") as f:
         f.write(private_key)
 else:
+    print("")
     print("#> Private key bestaat! skipping")
 
 if os.path.isfile("./public.pem") == False:
@@ -23,11 +24,33 @@ if os.path.isfile("./public.pem") == False:
     with open("public.pem", "wb") as f:
         f.write(public_key)
 else:
+    print("")
     print("#> Public key bestaat! skipping")
+    
+if os.path.isfile("./fkey.key") == False:
+    newfkey = Fernet.generate_key()
+    with open("fkey.key", "wb") as f:
+        f.write(newfkey)
+else:
+    print("")
+    print("#> Encryption key bestaat! skipping")
 
+fkey = open('fkey.key').read()
+fernet = Fernet(fkey)
+encMessage = fernet.encrypt(message)
 key = RSA.import_key(open('private.pem').read())
-hash = SHA256.new(message)
+hash = SHA256.new(encMessage)
 signature = pkcs1_15.new(key).sign(hash)
+fkey = open('fkey.key').read()
+
+print("")
+print("#> Encyption Key:", fkey)
+
+print("")
+print("#> Verleuteld Bericht:", encMessage)
+
+with open("message.encrypted", "wb") as f:
+    f.write(encMessage)
 
 with open("message.signature", "wb") as message_sig:
     message_sig.write(signature)
--- Automation/Week 2/Opdracht 3/verify_by_alice.py
+++ Automation/Week 2/Opdracht 3/verify_by_alice.py
@@ -1,19 +1,25 @@
 from Crypto.Signature import pkcs1_15
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
+from cryptography.fernet import Fernet
 
-with open("message.txt", "rb") as message_data:
-    message = message_data.read()
-    print(message.strip())
-    
+fkey = open('fkey.key').read()
+fernet = Fernet(fkey)
+
+with open("message.encrypted", "rb") as message_data:
+    encmessage = message_data.read()
+    message = fernet.decrypt(encmessage).decode()
+    print("#> Bericht van Bob:", message.strip())
+
 with open("message.signature", "rb") as signature_data:
     signature = signature_data.read()
 
+#Checken of er gekloot is met het bestand
 key = RSA.import_key(open('public.pem').read())
-h = SHA256.new(message)
+h = SHA256.new(encmessage)
 
 try:
     pkcs1_15.new(key).verify(h, signature)
-    print("The signature is valid.")
+    print("#> The signature is valid.")
 except (ValueError, TypeError):
-   print("The signature is not valid.")
+   print("#> The signature is not valid.")
